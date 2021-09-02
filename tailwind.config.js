77a5a56a2fb536676246886ff6565fba176dc755
Nikhil Vats <nikhilvats@Nikhils-MacBook-Pro.local>
2021-09-03 02:09:32 +0530

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,25 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          primary: '#032C28',
+          secondary: '#0F524B',
+          faded: '#0f524b1a'
+        },
+        yellow: {
+          primary: '#CB912C',
+          faded: '#F5F3F0'
+        }
+      },
+      boxShadow: {
+        custom: '0px 34px 44px rgba(229, 224, 217, 0.6)'
+      },
+      gridTemplateColumns: {
+        custom: '1fr auto'
+      }
+    },
   },
   variants: {
     extend: {},
